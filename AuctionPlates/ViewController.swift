f09cf160e92301bbdddabb0a9d027a289f4bf220
hnam2711 <namcuong2711@gmail.com>
2024-04-20 17:07:51 +0700

    Init non-storyboard project

---
+++ AuctionPlates/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  AuctionPlates
+//
+//  Created by Hai Nam on 20/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
